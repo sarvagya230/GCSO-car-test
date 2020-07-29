331c0d31a967a542ccc75ec90a88858a3c62d1c9
sarvagya230 <68740017+sarvagya230@users.noreply.github.com>
2020-07-29 20:26:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var car,wall,speed,sizeH,sizeW,weight;
+weight=random(400,500);
+var Destruction ;
+function setup() {
+  speed=random(1,7);
+  sizeH=random(20,50);
+  sizeW=sizeH;
+  car=createSprite(100,100,20,20);
+  wall=createSprite(900,100,30,90);
+  createCanvas(1600,1000);
+  car.shapeColor="white";
+
+}
+
+function draw() {
+  
+ 
+  background(0);
+  car.width=sizeW;
+  car.height=sizeH;
+ 
+  car.velocityX=speed;
+  deformation(car,wall)
+  console.log(destruction);
+  
+  
+  
+  drawSprites();
+}
+function deformation(a,b)
+{
+  if(a.collide(b))
+  {
+    if(Destruction<100)
+    {
+      a.shapeColor=color(255,0,0)
+    }
+    else if(Destruction<180 && Destruction>100)
+    {
+      a.shapeColor=color(0,255,0);
+    }
+    else
+    {
+      a.shapeColor=color(0,0,255);
+    }
+  }
+
+
+}
+function destruction()
+{
+  Destruction=0.5*weight*speed*speed;
+}
